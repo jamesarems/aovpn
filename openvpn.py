b8556bf6883498a6776ec52d896deb554054f718
James PS <jamesarems@gmail.com>
2018-12-12 16:37:26 +0400

    Update openvpn.py

--- openvpn.py
+++ openvpn.py
@@ -1,5 +1,6 @@
+#!/bin/python
 ###Freedom along with security for Cyber
-## OpenVPN Server (Hard - Version)
+## Opennebula Server (Hardening - Version)
 ## Author : James PS
 ## Email  : jamesarems@hotmail.com
 ## Git    : github.com/jamesarems
@@ -9,11 +10,19 @@ import os,sys,subprocess
 uid = os.popen('id -u').read(1)
 ipf = os.popen('more /proc/sys/net/ipv4/ip_forward').read(1)
 ovpn = os.popen('rpm -q openvpn').read(7)
+osvr = os.popen('rpm -qf /etc/redhat-release').read(16)
+########################
+try:
+    inputd = sys.argv[1]
+except IndexError:
+    inputd = 'null'
+########################
+
 
 def checkRoot():
 	os.system('clear')
 	if ( uid == "0" ):
-		cleanAll()
+		osCheck()
 		#print('Ok aanutto')
 	else:
 		print('\x1b[0;37;41m' +'You are not root user' + '\x1b[0m')
@@ -34,7 +43,7 @@ def createCerts():
 	os.system('/usr/share/easy-rsa/3/easyrsa build-ca nopass')
 	os.system('/usr/share/easy-rsa/3/easyrsa gen-dh')
 	os.system('/usr/share/easy-rsa/3/easyrsa build-server-full server nopass')
-	os.system('/usr/share/easy-rsa/3/easyrsa build-client-full client nopass')
+	#os.system('/usr/share/easy-rsa/3/easyrsa build-client-full client nopass')
 	os.system('/usr/share/easy-rsa/3/easyrsa gen-crl')
 	os.system('openvpn --genkey --secret pki/ta.key')
 	copyConf()
@@ -81,12 +90,54 @@ def cleanAll():
 			sys.exit(1)
 	else:
 		cleanAllMain()
-			
+
+def osCheck():
+	if ( osvr == "centos-release-7" ):
+		inputCheck()
+	else:
+		print('Your Operating system is not supported')
+		sys.exit(1)	
 
 def cleanAllMain():
 	print('\x1b[1;33;41m' +'CLEANING EXISTING SERVICES AND FILES----->'+ '\x1b[0m')
-	os.system('yum remove -y openvpn easy-rsa iptables iptables-services wget yum-cron net-tools bind-utils nc mtr; rm -rf /etc/openvpn ;rm -rf firewall.sh; rm -rf pki ; rm -rf /tmp/server.log ; iptables -F ; iptables-save')
+	os.system('yum remove -y openvpn easy-rsa iptables iptables-services wget yum-cron net-tools bind-utils nc mtr; rm -rf /etc/openvpn ;rm -rf firewall.sh; rm -rf pki ; rm -rf /tmp/server.log')
 	depCheck()
+
+def inputCheck():
+	if ( inputd == "client"):
+		clientConf()
+	elif ( inputd == "server" ):
+		cleanAll()
+	else:	
+		os.system('clear')
+		print('\x1b[0;37;44m' +'OpenVPN.py'+ '\x1b[0m' ', 2018 Developed by James PS. ')
+		print('Options ------> help , server, client, advance-server, advance-client')
+		sys.exit(1)
+
+def clientConf():
+	print('\x1b[6;30;42m' +'CREATING CLIENT CERTIFICATES------>'+ '\x1b[0m')
+	clientName = raw_input('Input user/client name: ')
+	serverIp = raw_input('Input VPN Server IP: ')
+	if not clientName:
+		print('ERROR : YOU MUST PUT USER/CLIENT NAME..!')
+		sys.exit(1)
+	elif not serverIp:
+		print('ERROR : YOU MUST PUT SERVER IP..!')
+                sys.exit(1)
+	else:
+		os.system('rm -rf %s; rm -rf %s.tar' % (clientName, clientName) )
+		os.system('rm -rf pki/issued/%s.crt;rm -rf pki/private/%s.key; rm -rf pki/reqs/%s.req;sed -i "/%s/d" pki/index.txt ' % (clientName, clientName, clientName, clientName) )
+		os.system('/usr/share/easy-rsa/3/easyrsa build-client-full %s nopass' % (clientName) )
+		os.system('mkdir %s' % (clientName) )
+		os.system('cp pki/ca.crt %s/ca.crt' % (clientName) )
+		os.system('cp pki/issued/%s.crt %s/client.crt' % (clientName, clientName) )
+		os.system('cp pki/private/%s.key %s/client.key' % (clientName, clientName) )
+		os.system('cp pki/ta.key %s/ta.key' % (clientName) )
+		os.system('cp client-sample.ovpn %s/client.ovpn' % (clientName) )
+		os.system("sed -i 's/serverip/%s/g' %s/client.ovpn " % (serverIp, clientName))
+		os.system('tar cfz %s.tgz %s ' % (clientName, clientName))
+		print('YOUR CLIENT FILE IS READY AS %s' % (clientName))
+		sys.exit(1)
 
 
 checkRoot()
